07fb8b1e3a0c1c7058314701dfee6c603a568bbb
ya-dvorovenko <yadvorovenko@gmail.com>
2018-01-27 12:02:52 +0200

    added linter

--- src/app.js
+++ src/app.js
@@ -3,6 +3,6 @@ import { PORT } from './scripts/config';
 import http from 'http';
 
 http.createServer((req, res) => {
-    res.write('Hello from Node server');
-    res.end();
+  res.write('Hello from Node server');
+  res.end();
 }).listen(PORT);
